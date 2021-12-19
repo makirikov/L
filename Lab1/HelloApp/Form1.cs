cbf1d837395a480ead2938f61711e4ad5da7fd61
makirikov <95547054+makirikov@users.noreply.github.com>
2021-12-19 19:59:04 +0300

    Add files via upload

---
+++ Lab1/HelloApp/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace HelloApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            button1.Click += button1_Click;
+        }
+
+        private void button1_Click(object? sender, EventArgs e)
+        {
+            MessageBox.Show("Привет");
+        }
+    }
+}
